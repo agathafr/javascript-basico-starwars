d32e5008f61a13141035634dc742e955a8233326
Agatha França <agathafranca@outlook.com>
2023-04-11 08:04:32 -0300

    Manipula html através do javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('Olá, Javascript!')
+
+var userName = 'Fernando Papito'
+
+document.getElementById('user-name').innerHTML = userName
+
+/* var nome = 'Mestre Yoda'
+var idade = 100
+var jedi = true
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof jedi) */
