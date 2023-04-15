7eb20746348b7971820e05d88cf36b4f826e7066
Agatha França <agathafranca@outlook.com>
2023-04-15 17:15:40 -0300

    Exemplifica controle de fluxo e bdd

--- js/app.js
+++ js/app.js
@@ -53,9 +53,49 @@ soma(5, 5) */
 
 boasVindas('Agatha') */
 
-function soma(n1, n2) {
+/* function soma(n1, n2) {
     return n1 + n2
 }
 
 const resultado = soma (5, 5)
-console.log(resultado)
+console.log(resultado) */
+
+// Controle de fluxos //
+
+// Sendo um cliente correntista do banco
+// Posso sacar dinheiro em caixas eletrônicos
+// Para poder comprar em lugares que não aceitam o cartão de débito ou crédito
+
+var saldo = 1000
+
+function saque(valor) {
+    if (valor > saldo) {
+        console.log('O valor é superior ao saldo')
+    } else if (valor > 700) {
+        console.log('O valor é superior ao máximo permitido por operação')
+    } else {
+        saldo = saldo - valor
+    }
+}
+
+saque(701)
+
+console.log(saldo)
+
+// Cenário 1: Saque com sucesso
+// Dado que meu saldo é de 1000 reais
+// Quando faço um saque de 500 reais
+// Então o valor do saque deve ser deduzido do meu saldo 
+
+// Cenário 2: Saque com valor superior ao saldo
+// Dado que meu saldo é de 1000 reais
+// Quando faço um saque de 1001 reais
+// Então não deve deduzir do meu saldo
+// E deve mostrar uma mensagem de alerta informando que o valor é superior ao saldo
+
+// Cenário 3: Saque com valor máximo
+// Dado que meu saldo é de 1000 reais
+// E o valor máximo por operação é de 700
+// Quando faço um saque no valor de 701 reais
+// Então não deve deduzir do meu saldo
+// E deve mostrar uma mensagem de alerta informando que o valor é superior ao máximo permitido por operação
