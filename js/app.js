00813c2da337af204dfdd16bedb86be449840872
Agatha França <agathafranca@outlook.com>
2023-04-15 16:31:17 -0300

    Exemplifica operador de comparação

--- js/app.js
+++ js/app.js
@@ -16,17 +16,25 @@ console.log(typeof jedi) */
 
 // Operadores matemáticos //
 
-var n1 = 7
+/* var n1 = 7
 var n2 = 2.5
 
 console.log(typeof n1)
-console.log(typeof n2)
+console.log(typeof n2) */
 
 // + para somar
 // - para subtrair
 // * para multiplicar
 // / para dividir
 
+/* 
 var total = n1 / n2
+console.log(total) */
 
-console.log(total)
+// Operadores de comparação
+
+var v1 = 5
+var v2 = '5'
+
+var resultado = v1 !== v2
+console.log(resultado)
