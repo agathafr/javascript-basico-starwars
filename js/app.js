b0c0e76c4b20850ed2fe869812ff587118bddca2
Agatha França <agathafranca@outlook.com>
2023-04-15 16:24:21 -0300

    Apresenta operadores matemáticos

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var userName = 'Fernando Papito'
 
 document.getElementById('user-name').innerHTML = userName
 
+// Variáveis //
+
 /* var nome = 'Mestre Yoda'
 var idade = 100
 var jedi = true
@@ -11,3 +13,20 @@ var jedi = true
 console.log(typeof nome)
 console.log(typeof idade)
 console.log(typeof jedi) */
+
+// Operadores matemáticos //
+
+var n1 = 7
+var n2 = 2.5
+
+console.log(typeof n1)
+console.log(typeof n2)
+
+// + para somar
+// - para subtrair
+// * para multiplicar
+// / para dividir
+
+var total = n1 / n2
+
+console.log(total)
